98fc6e3224bdcbf28e6768d8cd716093ba3c7431
Sean R Reid <sean@torchcodelab.com>
2020-10-22 10:35:39 -0400

    Updated depencies

--- src/components/DemoComponent.jsx
+++ src/components/DemoComponent.jsx
@@ -1,26 +1,38 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import StateContext from "../context";
 
-const handleChange = (e, dispatch) => {
-  dispatch({
-    type: "CHANGE_NAME",
-    name: e.target.value,
-  });
-};
-
 const DemoComponent = () => {
+  const [input, setInput] = useState("");
   const [value, dispatch] = useContext(StateContext);
   const { name } = value;
+
+  const _handleChange = (name) => {
+    setInput(name);
+  };
+
+  const _handleSubmit = (e, dispatch) => {
+    e.preventDefault();
+    dispatch({
+      type: "CHANGE_NAME",
+      name: input,
+    });
+  };
+
   return (
     <>
-      <p>Hi, my name is {name}</p>
-      <form>
-        <input
-          type="text"
-          name="newName"
-          placeholder="New Name"
-          onChange={(e) => handleChange(e, dispatch)}
-        />
+      <p>Welcome {name}! We have such sights to show you!</p>
+      <form onSubmit={(e) => _handleSubmit(e, dispatch)}>
+        <label>
+          Enter a new name:
+          <input
+            type="text"
+            name="newName"
+            placeholder="New Name"
+            onChange={(e) => _handleChange(e.target.value)}
+            value={input}
+          />
+        </label>
+        <button type="submit">Submit</button>
       </form>
     </>
   );
